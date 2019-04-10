d758669d0ddfa428e6f5b42386f1305b222443a6
AJLambda <apj2277@gmail.com>
2019-04-10 12:36:28 -0700

    imported packages in index.js

--- src/index.js
+++ src/index.js
@@ -3,17 +3,22 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { Provider } from "react-redux";
-import { createStore } from "redux";
-import rootReducer from "./reducers";
-// needed dependancies
-// applyMiddleware from redux
-// thunk from redux-thunk
-// logger from redux-logger
-// rootReducer from ./reducers
 
+// needed dependancies
+
+// applyMiddleware from redux
+import { applyMiddleware, createStore } from "redux";
+// thunk from redux-thunk
+import thunk from "redux-thunk";
+// logger from redux-logger
+import logger from "redux-logger";
+// rootReducer from ./reducers
+import charsReducer from "./reducers";
+
+//We're going to use applyMiddleware to inject middleware into the store, specifically we'll add the redux-thunk and redux-logger middleware packages.
 const store = createStore(
-  rootReducer
-  /* applyMiddleware goes here */
+  charsReducer,
+  /* applyMiddleware goes here */ applyMiddleware(thunk, logger)
 );
 
 ReactDOM.render(
